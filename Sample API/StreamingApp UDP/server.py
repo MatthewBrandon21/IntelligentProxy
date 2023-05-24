d4b877e49025a1445da58e8f116c6231e0ccd371
Matthew Brandon Dani <matthew.brandon@student.umn.ac.id>
2023-05-25 00:36:05 +0700

    video streaming app

--- Sample API/StreamingApp UDP/server.py
+++ Sample API/StreamingApp UDP/server.py
@@ -17,7 +17,7 @@ BUFF_SIZE = 65536
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 host_name = socket.gethostname()
-host_ip = '192.168.0.103'#  socket.gethostbyname(host_name)
+host_ip = '192.168.29.1'#  socket.gethostbyname(host_name)
 print(host_ip)
 port = 9688
 socket_address = (host_ip,port)
@@ -57,57 +57,33 @@ def video_stream():
         msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
         print('GOT connection from ',client_addr)
         WIDTH=400
-        while(True):
-            frame = q.get()
-            encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
-            message = base64.b64encode(buffer)
-            server_socket.sendto(message,client_addr)
-            frame = cv2.putText(frame,'FPS: '+str(round(fps,1)),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-            if cnt == frames_to_count:
-                try:
-                    fps = (frames_to_count/(time.time()-st))
-                    st=time.time()
-                    cnt=0
-                    if fps>FPS:
-                        TS+=0.001
-                    elif fps<FPS:
-                        TS-=0.001
-                    else:
-                        pass
-                except:
+        frame = q.get()
+        encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+        message = base64.b64encode(buffer)
+        server_socket.sendto(message,client_addr)
+        frame = cv2.putText(frame,'FPS: '+str(round(fps,1)),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        if cnt == frames_to_count:
+            try:
+                fps = (frames_to_count/(time.time()-st))
+                st=time.time()
+                cnt=0
+                if fps>FPS:
+                    TS+=0.001
+                elif fps<FPS:
+                    TS-=0.001
+                else:
                     pass
-            cnt+=1
-            cv2.imshow('TRANSMITTING VIDEO', frame)
-            key = cv2.waitKey(int(1000*TS)) & 0xFF	
-            if key == ord('q'):
-                os._exit(1)
-                TS=False
-                break
-
-def audio_stream():
-    s = socket.socket()
-    s.bind((host_ip, (port-1)))
-    s.listen(5)
-    CHUNK = 1024
-    wf = wave.open("temp.wav", 'rb')
-    p = pyaudio.PyAudio()
-    print('server listening at',(host_ip, (port-1)))
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    input=True,
-                    frames_per_buffer=CHUNK)
-    client_socket,addr = s.accept()
-    while True:
-        if client_socket:
-            while True:
-                data = wf.readframes(CHUNK)
-                a = pickle.dumps(data)
-                message = struct.pack("Q",len(a))+a
-                client_socket.sendall(message)
+            except:
+                pass
+        cnt+=1
+        cv2.imshow('TRANSMITTING VIDEO', frame)
+        key = cv2.waitKey(int(1000*TS)) & 0xFF	
+        if key == ord('q'):
+            os._exit(1)
+            TS=False
+            break
 
 from concurrent.futures import ThreadPoolExecutor
 with ThreadPoolExecutor(max_workers=3) as executor:
-    executor.submit(audio_stream)
     executor.submit(video_stream_gen)
     executor.submit(video_stream)
