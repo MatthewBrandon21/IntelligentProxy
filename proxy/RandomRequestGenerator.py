78c993b8039cfd98df21d987555bef012c958d0d
Matthew Brandon Dani <matthew.brandon@student.umn.ac.id>
2023-05-24 07:53:34 +0700

    Random Request Generator

--- proxy/RandomRequestGenerator.py
+++ proxy/RandomRequestGenerator.py
@@ -1,7 +1,19 @@
 import requests
+from sys import argv
+import sys
+import platform
+import threading
 import time
 import random
 import json
+
+system = platform.uname()[0]
+End = '\033[0m'
+help_command = """
+Random Request Generator:
+          ./RandomRequestGenerator.py <Time (s)> <threadCount>
+          --help
+"""
 
 try:
     allEndpoints = {}
@@ -72,11 +84,47 @@ try:
 except FileNotFoundError:
     print("Endpoints.json file not found!")
 
-print(allEndpoints)
-url = allEndpoints[3]["protocol"] + "://" + allEndpoints[3]["ip"] + ":" + str(allEndpoints[3]["port"])+ allEndpoints[3]["url"]
-print(url)
-
-myobj = {'data': 'dataku'}
-# response = requests.get(url)
-response = requests.post(url, json=myobj, timeout=2.50, stream=True)
-print(response.content)
+def main():
+    if argv[1] == argv[1]:
+        try:
+            time_input = int(argv[1])
+            thread_count = int(argv[2])
+            t_end = time.time() + time_input
+            time.sleep(0.35)
+            print("\nUsage: Ctrl + C To Exit!!!\n")
+            def run(h):
+                global allEndpoints
+                while time.time() < t_end:
+                    try:
+                        x = random.randint(1, len(allEndpoints))
+                        url_request = allEndpoints[x]["protocol"] + "://" + allEndpoints[x]["ip"] + ":" + str(allEndpoints[x]["port"])+ allEndpoints[x]["url"]
+                        start_time = time.perf_counter()
+                        if(allEndpoints[x]["type"] == "GET"):
+                            response = requests.get(url_request, timeout=2.50)
+                        else:
+                            response = requests.post(url_request, json=allEndpoints[x]["obj"], timeout=2.50, stream=True)
+                        print(f"\nPacket send to {url_request}, response time: {(time.perf_counter()-start_time)*1000} ms")
+                        print(response.content)
+                    except Exception as e:
+                        print(f"Send packet error, error : {e}")
+                    
+                    # Random sleep time from 0 to 10 seconds
+                    time.sleep(random.randint(0, 10))
+            for i in range(thread_count):
+                t = threading.Thread(target=run, args=[i])
+                t.start()
+        except KeyboardInterrupt:
+            print("\nCtrl + C")
+            print("\nStop " + "Generating !!!\n" + End)
+            sys.exit()
+    elif argv[1] == '--help':
+        print(help_command)
+        sys.exit()
+    else:
+        print("{} Not Found!".format(argv[1]))
+        sys.exit()
+if __name__ == '__main__':
+    try:
+        main()
+    except IndexError:
+        print(help_command)
