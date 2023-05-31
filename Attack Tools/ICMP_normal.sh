aba5385e61df4ddce62c1f18770396f37fa81855
Matthew Brandon Dani <matthew.brandon@student.umn.ac.id>
2023-05-31 10:35:05 +0700

    icmp normal generator

--- Attack Tools/ICMP_normal.sh
+++ Attack Tools/ICMP_normal.sh
@@ -2,10 +2,10 @@
 
 #Simulate Normal Traffic
 while true; do
-	packet=$(shuf -i 10-20 -n 1)
-	bytes=$(shuf -i 150-200 -n 1)
+	packet=$(shuf -i 1-20 -n 1)
+	bytes=$(shuf -i 64-200 -n 1)
 	pause=$(shuf -i 1-5 -n 1)
-	sudo hping3 -c $packet -d $bytes -s 80 -k 192.168.29.128
+	sudo hping3 -c $packet -d $bytes --icmp 192.168.29.128
 	sleep $pause
 done
 	
