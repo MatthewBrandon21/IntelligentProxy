64ce72a93b64fdca25a7823a6a5411a82e778ff4
Matthew Brandon Dani <matthewbrandondani@gmail.com>
2023-10-26 13:15:34 +0700

    Update FastAPI_Backend.py

--- proxy/FastAPI_Backend.py
+++ proxy/FastAPI_Backend.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+#uvicorn main:app --reload
+#pip install "fastapi[all]"
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+  return {"message": "Hello from FastAPI"}
+
+@app.get("/home", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("page2.html", {"request": request})
